0b00e5ac4c987dab8b4f3f8359ff33c9ef348e48
aravind-kumar <karavindkumar1993@gmail.com>
2018-01-25 23:21:59 -0500

    Incomplete have to fix

--- rust_guess.rs
+++ rust_guess.rs
@@ -1,9 +1,12 @@
-
+extern crate rand;
 
 use std::io;
+use rand::Rng;
 
 fn main() {
-
+ 
+   let rand_num = rand::thread_rng().gen_range(1,100);
+   println!("The secret number is {}",rand_num);
    println!("Enter the guess number");
    let mut guess_num = String::new();
    io::stdin().read_line(&mut guess_num).expect("Please enter a valid value");
